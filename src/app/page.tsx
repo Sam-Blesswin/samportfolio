6db25c5108dc6b8447a7614180a3123cad142225
Sam <samblesswinedu@gmail.com>
2024-02-25 10:09:14 -0700

    Fix for deployment issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,13 @@ import { tabItems } from "@/types/FormTypes";
 
 async function extractAllDatas(currentSection: tabItems) {
   try {
-    const url =
-      process.env.NODE_ENV === "development"
-        ? `${process.env.API_URL}/api/${currentSection}/get`
-        : `/api/${currentSection}/get`;
-
-    const response = await fetch(url, {
-      method: "GET",
-      cache: "no-store",
-    });
+    const response = await fetch(
+      `${process.env.URL}/api/${currentSection}/get`,
+      {
+        method: "GET",
+        cache: "no-store",
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Failed to fetch data: ${response.statusText}`);
