354f59d572476e1b09877b3ffa243335ba936fc5
Sam <samblesswinedu@gmail.com>
2024-03-13 11:20:30 -0600

    Added CORS and removed logs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://samblesswin-portfolio.vercel.app/",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: ["drive.google.com"],
+  },
+  compiler: {
+    removeConsole: true,
   },
 };
 
